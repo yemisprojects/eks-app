d9346269574df0ae1c1c2d93140cdce6f4fccc0e
Yemi <yemisiomonijo20@yahoo.com>
2023-11-09 13:11:57 -0500

    add vpc

--- main.tf
+++ main.tf
@@ -1,2 +1,43 @@
-# data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {}
 # data "aws_caller_identity" "current" {}
+
+locals {
+
+  name = "ex-${replace(basename(path.cwd), "_", "-")}"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 48)]
+
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+    "Type"                   = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+    # Tags subnets for Karpenter auto-discovery
+    "karpenter.sh/discovery" = local.name
+    "Type"                   = "private-subnet"
+
+  }
+
+  tags                    = local.tags
+  map_public_ip_on_launch = true
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,33 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_name" {
+  description = "name of EKS VPC"
+  type        = string
+  default     = "eksvpc"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR range"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_single_nat_gateway" {
+  description = "Enable only single NAT Gateway in one Availability Zone to save costs during our demos"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT Gateways for Private Subnets Outbound Communication"
+  type        = bool
+  default     = true
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "prod"
+}
